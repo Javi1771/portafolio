015ed2d4112c150e93b5dae0395263eb531d48e6
Javi1771 <jl728122@gmail.com>
2025-10-17 19:40:09 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -67,7 +67,7 @@ export default function HomePage() {
                 Tecnologías
               </h3>
               <div className="flex flex-wrap gap-3">
-                {["Next.js", "Flutter", "SQL Server", "Firebase", "IoT", "Tailwind", "SQLite", "Google Maps", "Google Climate"].map(
+                {["Next.js", "Flutter", "SQL Server", "Firebase", "Tailwind", "Google Maps Plattform", "Y Más"].map(
                   (tech, index) => (
                     <span
                       key={tech}
