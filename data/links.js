3a9941bb82060eda8ca5cde0d58f6ce8f144b74e
Javi1771 <jl728122@gmail.com>
2026-01-20 21:17:33 -0600

    Update links.js

--- data/links.js
+++ data/links.js
@@ -1,4 +1,4 @@
 export const GITHUB_ACCOUNTS = ['Javi1771', 'JavierUTSJR'];
 export const WHATSAPP = '524481264193';
 export const EMAIL = 'jl728122@gmail.com';
-export const LINKEDIN = 'www.linkedin.com/in/javier-lópez-camacho-9a4352253';
+export const LINKEDIN = 'www.linkedin.com/in/javier-lópez-camacho';
